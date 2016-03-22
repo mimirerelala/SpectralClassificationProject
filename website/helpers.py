44e556c3b23f058852cb3128ed664d7a8f8ce3c8
yaggul <krommul@gmail.com>
2016-03-23 01:28:57 +0200

    adding multiple file upload to unique folder, multiple plot with legend, real filenames, upload folder created on per upload basis

--- website/helpers.py
+++ website/helpers.py
@@ -4,46 +4,57 @@ from bokeh.plotting import figure
 from bokeh.resources import INLINE
 from bokeh.embed import components
 from starspectre.settings import BASE_DIR as root
-colors = ['green', 'red']
+colors = ['green', 'red', 'orange', 'blue', 'black', 'yellow', 'brown', 'grey']
 temp = os.path.join(root, 'website/temp')
 
 
-def handle_uploaded_file(file, filename):
-    with open(os.path.join(temp, filename), 'wb+') as dataout:
+def handle_uploaded_file(foldername, file, filename):
+    os.chdir(temp)
+    try:
+        os.mkdir(foldername)
+    except:
+        pass
+    session_folder = os.path.join(temp, foldername)
+    with open(os.path.join(session_folder, filename), 'wb+') as dataout:
         for chunk in file.chunks():
             dataout.write(chunk)
 
 
-def file_parse(folder, file_name):
+def file_parse(folder):
     for_plot = []
     for file in os.listdir(folder):
-        if file.startswith(file_name):
             for_plot.append(os.path.join(folder, file))
     return for_plot
 
 
 def add_coords(filename):
-    coords = []
+    '''
     data_x, data_y = np.loadtxt(filename, usecols=(0,1), unpack=True)
+    '''
+    coords = []
+    data = np.genfromtxt(filename, dtype=None)
+    data_x = data[:, 0]
+    data_y = data[:, 1]
     coords.append(data_x)
     coords.append(data_y)
     return coords
 
 
-def gen_coordinates(folder, file_name):
+def gen_coordinates(folder):
     coordinates = []
-    for i in file_parse(folder, file_name):
+    for i in file_parse(folder):
         a = add_coords(i)
         coordinates.append(a)
-    return coordinates
+    return [coordinates, file_parse(folder)]
 
 
-def plot_handle(coordinates):
-    PLOT_OPTIONS = dict(plot_width=800, plot_height=400)
-    plot = figure(responsive=True, tools='reset,box_zoom,wheel_zoom,pan,resize,save', **PLOT_OPTIONS)
+def plot_handle(data_list):
+    coordinates = data_list[0]
+    files = [os.path.basename(i) for i in data_list[1]]
+    PLOT_OPTIONS = dict(plot_width=800, plot_height=400, toolbar_location="left")
+    plot = figure(responsive=True, tools='reset,box_zoom,wheel_zoom,pan,resize,save,hover', **PLOT_OPTIONS)
     for index in range(len(coordinates)):
-        plot.line(coordinates[index][0], coordinates[index][1], line_color='{}'.format(colors[index]))
-
+        plot.line(coordinates[index][0], coordinates[index][1], line_color='{}'.format(colors[index]), legend='{}'.format(files[index]))
 
     resources = INLINE
     js_resources = resources.render_js()
@@ -51,6 +62,3 @@ def plot_handle(coordinates):
     script, div = components({'plot': plot})
 
     return (js_resources, css_resources, script , div)
-
-
-
--- website/views.py
+++ website/views.py
@@ -1,14 +1,14 @@
 import os
 from random import choice
 from string import ascii_letters
-import numpy as np
+# import numpy as np
 from django.shortcuts import render
 from django.http import HttpResponse
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.embed import components
+# from bokeh.plotting import figure
+# from bokeh.resources import INLINE
+# from bokeh.embed import components
 from . sshkey import ssh_key
-from starspectre.settings import BASE_DIR as root
+# from starspectre.settings import BASE_DIR as root
 from website import forms as f
 from website import helpers as h
 filename = 0
@@ -26,20 +26,23 @@ def smart_chart(request):
         if request.POST.get('postname') == 'FILEPOST':
             form = f.UploadFileForm(request.POST, request.FILES)
             # comment right before first + sign if we are OK! with file overwrite for every session
-            filename = request.POST.get('csrfmiddlewaretoken') + '-' + ''.join([i for j in range(5) for i in choice(ascii_letters) ])
-            if form.is_valid():
-                h.handle_uploaded_file(request.FILES['file'], filename)
-                feedback = 'Successfully uploaded'
-                js_resources, css_resources, script, div = h.plot_handle(h.gen_coordinates(h.temp, filename))
-            else:
-                feedback = 'Error uploading'
-                filename = 0
+            foldername = request.POST.get('csrfmiddlewaretoken') + '-' + ''.join([i for j in range(5) for i in choice(ascii_letters) ])
+            # if form.is_valid():
+            file_list = request.FILES.getlist('File')
+            for file in file_list:
+                filename = file.name
+                h.handle_uploaded_file(foldername, file, filename)
+            # feedback = 'Successfully uploaded'
+            js_resources, css_resources, script, div = h.plot_handle(h.gen_coordinates(os.path.join(h.temp, foldername)))
+            # else:
+            #     feedback = 'Error uploading'
+            #     filename = 0
         elif request.POST.get('postname') == 'CLASSIFY':
             if filename != 0:
                 feedback = 'Succsessfully plotting'
-                js_resources, css_resources, script, div = h.plot_handle(h.gen_coordinates(h.temp, filename))
-                #form = f.UploadFileForm()
-                #filename = 0
+                js_resources, css_resources, script, div = h.plot_handle(h.gen_coordinates(os.path.join(h.temp, foldername)))
+                form = f.UploadFileForm()
+                filename = 0
             else:
                 feedback = 'No data submitet for analisys'
                 form = f.UploadFileForm()
